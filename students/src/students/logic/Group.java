ec35ccc261dc9642b3ef47f1dbfd6aa27387a8b3
oKolobaev <o.kolobaiev@gmail.com>
2019-07-15 23:56:24 +0300

    iss2

--- students/src/students/logic/Group.java
+++ students/src/students/logic/Group.java
@@ -2,48 +2,45 @@ package students.logic;
 
 public class Group {
 
-    // поле ИД ГРУППЫ
     private int groupId;
-    // поле ИМЯ ГРУППЫ
     private String nameGroup;
-    // поле КУРАТОР
     private String curator;
-    // поле СПЕЦИАЛЬНОСТЬ
     private String speciality;
-
-    public int getGroupId() {
-        return groupId;
-    }
-
-    public String getNameGroup() {
-        return nameGroup;
-    }
 
     public String getCurator() {
         return curator;
-    }
-
-    public String getSpeciality() {
-        return speciality;
-    }
-
-    public void setGroupId(int groupId) {
-        this.groupId = groupId;
-    }
-
-    public void setNameGroup(String nameGroup) {
-        this.nameGroup = nameGroup;
     }
 
     public void setCurator(String curator) {
         this.curator = curator;
     }
 
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getNameGroup() {
+        return nameGroup;
+    }
+
+    public void setNameGroup(String nameGroup) {
+        this.nameGroup = nameGroup;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
     public void setSpeciality(String speciality) {
         this.speciality = speciality;
     }
 
-    public String toString(){
+    public String toString() {
         return nameGroup;
     }
+
 }
--- students/src/students/logic/Student.java
+++ students/src/students/logic/Student.java
@@ -1,5 +1,7 @@
 package students.logic;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.Collator;
 import java.text.DateFormat;
 import java.util.Date;
@@ -7,97 +9,96 @@ import java.util.Locale;
 
 public class Student implements Comparable {
 
-    // поле ИД СТУДЕНТА
     private int studentId;
-    // поле ИМЯ
     private String firstName;
-    // поле ФАМИЛИЯ
     private String surName;
-    // поле ОТЧЕСТВО
     private String patronymic;
-    // поле ДАТА РОЖДЕНИЯ
     private Date dateOfBirth;
-    // поле ПОЛ
     private char sex;
-    // поле ИД ГРУППЫ
     private int groupId;
-    // поле ГОД ОБУЧЕНИЯ
     private int educationYear;
 
-    public void setStudentId(int studentId) {
-        this.studentId = studentId;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setSurName(String surName) {
-        this.surName = surName;
-    }
-
-    public void setPatronymic(String patronymic) {
-        this.patronymic = patronymic;
-    }
-
-    public void setDateOfBirth(Date dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public void setSex(char sex) {
-        this.sex = sex;
-    }
-
-    public void setGroupId(int groupId) {
-        this.groupId = groupId;
-    }
-
-    public void setEducationYear(int educationYear) {
-        this.educationYear = educationYear;
-    }
-
-    public int getStudentId() {
-        return studentId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getSurName() {
-        return surName;
-    }
-
-    public String getPatronymic() {
-        return patronymic;
+    public Student(ResultSet rs) throws SQLException {
+        setStudentId(rs.getInt(1));
+        setFirstName(rs.getString(2));
+        setPatronymic(rs.getString(3));
+        setSurName(rs.getString(4));
+        setSex(rs.getString(5).charAt(0));
+        setDateOfBirth(rs.getDate(6));
+        setGroupId(rs.getInt(7));
+        setEducationYear(rs.getInt(8));
     }
 
     public Date getDateOfBirth() {
         return dateOfBirth;
     }
 
-    public char getSex() {
-        return sex;
-    }
-
-    public int getGroupId() {
-        return groupId;
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
     }
 
     public int getEducationYear() {
         return educationYear;
     }
 
-    // DateFormat - класс для преобразования даты
-    // в строку в определеннном формате.
-    // Подробнее смотрите документацию по этому методу
+    public void setEducationYear(int educationYear) {
+        this.educationYear = educationYear;
+    }
+
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
     public String toString() {
         return surName + " " + firstName + " " + patronymic + ", "
                 + DateFormat.getDateInstance(DateFormat.SHORT).format(dateOfBirth)
                 + ", Группа ИД=" + groupId + " Год:" + educationYear;
     }
 
-    @Override
     public int compareTo(Object obj) {
         Collator c = Collator.getInstance(new Locale("ru"));
         c.setStrength(Collator.PRIMARY);
