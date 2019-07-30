78b87e3944ece285b3384979295cf6733ce57140
oKolobaev <o.kolobaiev@gmail.com>
2019-07-30 23:52:13 +0300

    servlet_2

--- students/src/students/web/HelloWorldServlet.java
+++ students/src/students/web/HelloWorldServlet.java
@@ -1,4 +1,7 @@
-package web;
+package students.web;
+
+import students.logic.Group;
+import students.logic.ManagementSystem;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -6,13 +9,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 public class HelloWorldServlet extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html;charset=utf-8");
 
         PrintWriter pw = resp.getWriter();
-        pw.println("<H1>Hello, world!</H1>");
-
+        pw.println("<B>Список групп</B>");
+        pw.println("<table border=1>");
+        try {
+            List<Group> l = ManagementSystem.getInstance().getGroups();
+            for (Group gr : l) {
+                pw.println("<tr>");
+                pw.println("<td>" + gr.getGroupId() + "</td>");
+                pw.println("<td>" + gr.getNameGroup() + "</td>");
+                pw.println("<td>" + gr.getCurator() + "</td>");
+                pw.println("<td>" + gr.getSpeciality() + "</td>");
+                pw.println("</tr>");
+            }
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+        pw.println("</table>");
     }
 }
